d01b08ad7c41ae46043c78c107d11dee0cc0795f
Talya D <talyadouek@gmail.com>
2018-04-10 23:45:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays (string) {
 return "Happy holidays!" ;
 }
 function happyHolidaysTo (name) {
-  return `Happy ${holiday}, ${name}` ;
+  return `Happy holidays, ${name}!`;
 }
 function holidayCountdown (days,holiday) {
   return `It's ${days} days until ${holiday}!` ;
